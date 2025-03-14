c0793b06db8bfcc46b68348d01df4ce4f0de11f9
Dan Hecht <dan.hecht@apple.com>
2025-03-14 10:46:33 -0700

    Fail FTL compile at end

--- Source/JavaScriptCore/dfg/DFGPlan.cpp
+++ Source/JavaScriptCore/dfg/DFGPlan.cpp
@@ -496,6 +496,7 @@ Plan::CompilationPath Plan::compileInThreadImpl()
 
         FTL::link(state);
         
+        state.allocationFailed = true;
         if (state.allocationFailed) {
             FTL::fail(state);
             return FTLPath;
