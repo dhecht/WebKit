fd87b980f41c9ea4c386d7aa0478b3c6d982d135
Dan Hecht <dan.hecht@apple.com>
2025-03-14 12:55:26 -0700

    Add option ftlFailCompileEnd

--- Source/JavaScriptCore/dfg/DFGPlan.cpp
+++ Source/JavaScriptCore/dfg/DFGPlan.cpp
@@ -498,8 +498,7 @@ Plan::CompilationPath Plan::compileInThreadImpl()
 
         FTL::link(state);
         
-        state.allocationFailed = true;
-        if (state.allocationFailed) {
+        if (state.allocationFailed || Options::ftlFailCompileEnd()) {
             FTL::fail(state);
             return FTLPath;
         }
