7eb574c312c2a6920b7255c9d76436af128580e7
Dan Hecht <dan.hecht@apple.com>
2025-12-16 19:59:54 -0800

    Cleanup

--- Source/JavaScriptCore/wasm/WasmOMGIRGenerator.cpp
+++ Source/JavaScriptCore/wasm/WasmOMGIRGenerator.cpp
@@ -143,7 +143,7 @@ public:
     {
         ASSERT(!isMaterialized());
         m_storage = std::bit_cast<uintptr_t>(var) | isMaterializedMask;
-        ASSERT(b3Variable() == var && isMaterialized());
+        ASSERT(isMateralized() && b3Variable() == var);
     }
 
     // Not named value() to disambiguate with TypedExpression::value()
