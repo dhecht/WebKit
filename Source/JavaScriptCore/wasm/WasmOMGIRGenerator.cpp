2b1b83bb3bbe5ba88f7e0b4950eea0f05fd9f7ee
Dan Hecht <dan.hecht@apple.com>
2026-04-15 12:12:51 -0700

    [JSC] Add per-function dump filtering to greedy register allocator https://bugs.webkit.org/show_bug.cgi?id=312400 rdar://174856004
    Reviewed by NOBODY (OOPS!).
    
    The --airGreedyRegAllocVerbose option is useful for detailed debugging
    but for large wasm modules, it produces too much output.
    
    So for understanding transformations the register allocator is performing
    on a particular function (especially for performance analysis), add
    the --airGreedyRegAllocDumpFunction=filter option to dump greedy register
    allocator state and IR for functions matching a substring filter.
    
    Also, attach the function simple name + hash to the register allocator stats
    output.
    
    To accomplish this, store the simple name + hash into the B3::Procedure.
    Also, consolidate IR dump points inside GreedyAllocator::run().
    
    * Source/JavaScriptCore/b3/B3Procedure.h:
    (JSC::B3::Procedure::setName):
    (JSC::B3::Procedure::name const):
    * Source/JavaScriptCore/b3/air/AirAllocateRegistersByGreedy.cpp:
    (JSC::B3::Air::Greedy::GreedyAllocator::run):
    (JSC::B3::Air::Greedy::GreedyAllocator::shouldDumpFunction const):
    (JSC::B3::Air::Greedy::GreedyAllocator::assignRegisters):
    (JSC::B3::Air::allocateRegistersByGreedy):
    * Source/JavaScriptCore/b3/air/AirPhaseStats.h:
    * Source/JavaScriptCore/b3/air/AirRegisterAllocatorStats.h:
    (JSC::B3::Air::AirAllocateRegistersStats::AirAllocateRegistersStats):
    * Source/JavaScriptCore/ftl/FTLState.cpp:
    (JSC::FTL::State::State):
    * Source/JavaScriptCore/runtime/OptionsList.h:
    * Source/JavaScriptCore/wasm/WasmOMGIRGenerator.cpp:
    (JSC::Wasm::parseAndCompileOMG):

--- Source/JavaScriptCore/wasm/WasmOMGIRGenerator.cpp
+++ Source/JavaScriptCore/wasm/WasmOMGIRGenerator.cpp
@@ -6312,6 +6312,7 @@ Expected<std::unique_ptr<InternalFunction>, String> parseAndCompileOMG(Compilati
     compilationContext.procedure = makeUniqueWithoutFastMallocCheck<Procedure>(info.usesSIMD(functionIndex));
 
     Procedure& procedure = *compilationContext.procedure;
+    procedure.setName(callee.nameWithHash());
     if (shouldDumpIRFor(functionIndex + info.importFunctionCount()))
         procedure.setShouldDumpIR();
 
